7fb448a36e6fdf86801351dc3571bb8966a85c7f
Constantine Melios <djagil@gmail.com>
2017-12-07 23:42:45 +0200

    end

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function nextQuote(q) {
     q.preventDefault();
     $.ajax({
-        url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"})
+        url: "https://random-quote-generator.herokuapp.com/api/quotes/random"})
         .done(function (data) {
-            $("#quote").html(data.content);
-            $("#author").html(data.title);
+            $("#quote").html(data.quote);
+            $("#author").html(data.author);
         });
 };
 
 function tweet() {
-    ;
+    var quote = $("#quote").text();
+    var author = $("#author").text();
+    location.href = `https://twitter.com/intent/tweet?hashtags=quote_of_the_day&text=${quote} 
+    -${author}`
 }
 
 $(document).ready(function() {
